1ea62977360d83a564e6750d7a3c54f83a27526b
MatthewOfomi <ofomimatthew7@gmail.com>
2025-02-20 23:52:23 +0100

    feat: habit model modified

--- habit/models.py
+++ habit/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from datetime import time
 
 
 class Habit(models.Model):
   name= models.CharField(max_length=255)
   description = models.TextField()
+  event_time = models.TimeField(default=time(7, 0)) 
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   
--- habit/serializers.py
+++ habit/serializers.py
@@ -5,5 +5,5 @@ from .models import Habit
 class HabitSerializer(serializers.ModelSerializer):
   class Meta:
     model = Habit
-    fields = ['id','name','description']
+    fields = ['name','description','event_time']
   
