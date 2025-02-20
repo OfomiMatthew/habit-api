26d77762a2fbb32c6323410246a5e8906561f721
MatthewOfomi <ofomimatthew7@gmail.com>
2025-02-20 22:54:09 +0100

    feat: requirements.txt file generated

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aou-)06f3&b!*-y^s^gfa^ic%6y0nv!4m)@ju=+-4%%9^1xy4s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['.vercel.app']
