78559a16b48c66487bb03813d9d32502dc2ae405
MatthewOfomi <ofomimatthew7@gmail.com>
2025-02-21 20:28:32 +0100

    feat: api modified

--- habit/serializers.py
+++ habit/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 from .models import Habit
 
-
 class HabitSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Habit
-    fields = ['name','description','event_time']
-  
+    event_time = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Habit
+        fields = ['name', 'description', 'event_time']
+
+    def get_event_time(self, obj):
+        return obj.event_time.strftime("%H:%M")  # Formats time to "HH:MM"
